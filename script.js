cebad30a701d3ff1f04b3b1165afd87e39604b4b
baba-jan <babajanbogavalli@gmail.com>
2025-01-31 12:50:50 +0530

    weather

--- script.js
+++ script.js
@@ -5,44 +5,16 @@ const condition = document.querySelector(".condition")
 const humidity = document.querySelector(".humidity")
 const windstatus = document.querySelector(".wind")
 const uvindex = document.querySelector(".uvindex")
-const sunriset = document.querySelector(".sun")
+const sunrise = document.querySelector(".sun")
+const sunset = document.querySelector(".record2")
 const visibility = document.querySelector(".visibility")
-const airquality=document.querySelector(".airqua")
 const cel = document.querySelector(".cel")
 const fah = document.querySelector(".fah")
 const perc = document.querySelector(".perc")
-const weeklyTem=document.querySelector(".temperature")
-const weeklyTem1=document.querySelector(".temperature1")
-const weeklyTem2=document.querySelector(".temperature2")
-const weeklyTem3=document.querySelector(".temperature3")
-const weeklyTem4=document.querySelector(".temperature4")
-const weeklyTem5=document.querySelector(".temperature5")
-const weeklyTem6=document.querySelector(".temperature6")
+const searchBtn=document.querySelector(".locbtn")
+const search=document.querySelector(".input")
 const locationElem = document.querySelector(".location")
-const todayTem=document.querySelector(".heat")
-const todayTem1=document.querySelector(".heat1")
-const todayTem2=document.querySelector(".heat2")
-const todayTem3=document.querySelector(".heat3")
-const todayTem4=document.querySelector(".heat4")
-const todayTem5=document.querySelector(".heat5")
-const todayTem6=document.querySelector(".heat6")
-const todayTem7=document.querySelector(".heat7")
-const todayTem8=document.querySelector(".heat8")
-const todayTem9=document.querySelector(".heat9")
-const todayTem10=document.querySelector(".heat10")
-const todayTem11=document.querySelector(".heat11")
-const todayTem12=document.querySelector(".heat12")
-const todayTem13=document.querySelector(".heat13")
-const todayTem14=document.querySelector(".heat14")
-const todayTem15=document.querySelector(".heat15")
-const todayTem16=document.querySelector(".heat16")
-const todayTem17=document.querySelector(".heat17")
-const todayTem18=document.querySelector(".heat18")
-const todayTem19=document.querySelector(".heat19")
-const todayTem20=document.querySelector(".heat20")
-const todayTem21=document.querySelector(".heat21")
-const todayTem22=document.querySelector(".heat22")
-const todayTem23=document.querySelector(".heat23")
+
 function newClock() {
   const timer = document.querySelector('.days');
   const currDate = new Date();
@@ -67,13 +39,22 @@ newClock();
 // code for location in search
 // 
 // Fetch current weather based on city or geolocation
-const city = "odisha"; // Replace with the city of your choice
+function fetchTodayWeather(){
+const city = "Bengaluru"; // Replace with the city of your choice
 const tempApiKey = "EJ6UBL2JEQGYB3AA4ENASN62J";
 const weatherApiUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=${tempApiKey}&contentType=json`;
 fetch(weatherApiUrl)
   .then((response) => response.json())
   .then((res) => {
     console.log(res);
+    const weeklyTem=document.querySelector(".temperature")
+    const weeklyTem1=document.querySelector(".temperature1")
+    const weeklyTem2=document.querySelector(".temperature2")
+    const weeklyTem3=document.querySelector(".temperature3")
+    const weeklyTem4=document.querySelector(".temperature4")
+    const weeklyTem5=document.querySelector(".temperature5")
+    const weeklyTem6=document.querySelector(".temperature6")
+
     //code for weekly temperature
     const dailyForecast = res.days;
     const dailyTemps = dailyForecast.slice(0, 7);
@@ -95,6 +76,30 @@ fetch(weatherApiUrl)
       }
     }
   //code for today timings temperature
+ const todayTem=document.querySelector(".heat")
+ const todayTem1=document.querySelector(".heat1")
+ const todayTem2=document.querySelector(".heat2")
+ const todayTem3=document.querySelector(".heat3")
+ const todayTem4=document.querySelector(".heat4")
+ const todayTem5=document.querySelector(".heat5")
+ const todayTem6=document.querySelector(".heat6")
+ const todayTem7=document.querySelector(".heat7")
+ const todayTem8=document.querySelector(".heat8")
+ const todayTem9=document.querySelector(".heat9")
+ const todayTem10=document.querySelector(".heat10")
+ const todayTem11=document.querySelector(".heat11")
+ const todayTem12=document.querySelector(".heat12")
+ const todayTem13=document.querySelector(".heat13")
+ const todayTem14=document.querySelector(".heat14")
+ const todayTem15=document.querySelector(".heat15")
+ const todayTem16=document.querySelector(".heat16")
+ const todayTem17=document.querySelector(".heat17")
+ const todayTem18=document.querySelector(".heat18")
+ const todayTem19=document.querySelector(".heat19")
+ const todayTem20=document.querySelector(".heat20")
+ const todayTem21=document.querySelector(".heat21")
+ const todayTem22=document.querySelector(".heat22")
+ const todayTem23=document.querySelector(".heat23")
   const timeForecast = res.days[0].hours;
     const dailyTemp = timeForecast.slice(0, 24);
     const hour = [
@@ -106,7 +111,7 @@ fetch(weatherApiUrl)
       const hourData = dailyTemp[i];
       const tempElement = hour[i];
       if (tempElement) {
-        tempElement.textContent = `${hourData.temp}°C`;
+        tempElement.textContent=`${hourData.temp}°C`;
       }
     }
  
@@ -128,8 +133,12 @@ fetch(weatherApiUrl)
     // to display uv Index
     const btn2=res.currentConditions
     displayUvIndex(btn2.index)
-    console.log(btn2.index);
+    // console.log(btn2.index);
     
+    // code to display the change icons
+    const btn3=res.currentConditions
+    changeIcon(btn3.icon)
+
     // code to uvindex
     const uvIndexValue = res.currentConditions.uvindex;
     displayUvIndex(uvIndexValue);
@@ -141,7 +150,10 @@ fetch(weatherApiUrl)
     //code to access visibility
     const visibilityValue1=res.currentConditions.visibility;
     displayVisibility(visibilityValue1)
-
+   
+    // code to access the Air Quality
+    const airQualityValue=res.currentConditions.airquality1
+    displayAirQuality(airQualityValue)
     //code for today highlights
     temp.textContent = `${temperatureCelsius}°C`;
     const percValue = res.currentConditions.precip;
@@ -150,15 +162,18 @@ fetch(weatherApiUrl)
     humidity.textContent = `${res.currentConditions.humidity}%`;
     windstatus.textContent = `${res.currentConditions.windspeed} km/h`;
     uvindex.textContent = `${res.currentConditions.uvindex}`;
-    sunriset.textContent = `${res.currentConditions.sunrise}`;
+    sunrise.textContent = `${res.currentConditions.sunrise} am`;
+    sunset.textContent=`${res.currentConditions.sunset} pm`
     visibility.textContent = `${res.currentConditions.visibility}`;
-    // airquality.textContent=`${res.currentConditions.airquality}`
+    //airquality.textContent=`${res.currentConditions.airquality}`
   })
   .catch((err) => {
     console.log(err);
   });
+}
+fetchTodayWeather()
 
-  // Get location (for geolocation-based weather)
+// Get location (for geolocation-based weather)
 fetch("https://geolocation-db.com/json/")
   .then((data1) => data1.json())
   .then((res1) => {
@@ -168,7 +183,16 @@ fetch("https://geolocation-db.com/json/")
     console.log(err1);
   });
 
-  // Convert Celsius to Fahrenheit
+// code for search button 
+searchBtn.addEventListener("click",()=>{
+  let city=search.value.trim()
+  if(city){
+    fetchTodayWeather(city)
+  }else{
+    alert("Please Enter Valid City Name !")
+  }
+})
+// Convert Celsius to Fahrenheit
 function celToFah(temperature) {
   return ((temperature * 9) / 5 + 32).toFixed(1);
 }
@@ -197,31 +221,31 @@ function  setCurrentIcon(icons){
   }
 }
 
-
 // code for icons according to weekly temperature
-// const weeklyIcon=document.querySelector(".img1")
-// const weeklyIcon1=document.querySelector(".img2")
-// const weeklyIcon2=document.querySelector(".img3")
-// const weeklyIcon3=document.querySelector(".img4")
-// const weeklyIcon4=document.querySelector(".img5")
-// const weeklyIcon5=document.querySelector(".img6")
-// const weeklyIcon6=document.querySelector(".img7")
-// function  changeIcon(icons){
-  // const currIcon =[
-  //   weeklyIcon,weeklyIcon1,weeklyIcon2,weeklyIcon3,weeklyIcon4,weeklyIcon5,weeklyIcon6
-  // ]
-//   if (icons === "partly-cloudy-day") {
-//       currIcon.src = "https://i.ibb.co/PZQXH8V/27.png";
-//   } else if (icons === "partly-cloudy-night") {
-//       currIcon.src = "https://i.ibb.co/Kzkk59k/15.png";
-//   } else if (icons === "rain") {
-//       currIcon.src = "https://i.ibb.co/kBd2NTS/39.png";
-//   } else if (icons === "clear-day") {
-//       currIcon.src = "https://i.ibb.co/rb4rrJL/26.png";
-//   } else if (icons === "clear-night") {
-//       currIcon.src = "https://i.ibb.co/1nxNGHL/10.png";
-//   }
-// }
+const weeklyIcon=document.querySelector(".img1")
+const weeklyIcon1=document.querySelector(".img2")
+const weeklyIcon2=document.querySelector(".img3")
+const weeklyIcon3=document.querySelector(".img4")
+const weeklyIcon4=document.querySelector(".img5")
+const weeklyIcon5=document.querySelector(".img6")
+const weeklyIcon6=document.querySelector(".img7")
+function  changeIcon(icons){
+  const currIcon =[
+    weeklyIcon,weeklyIcon1,weeklyIcon2,weeklyIcon3,weeklyIcon4,weeklyIcon5,weeklyIcon6
+  ]
+  if (icons === "partly-cloudy-day") {
+      currIcon.src = "https://i.ibb.co/PZQXH8V/27.png";
+  } else if (icons === "partly-cloudy-night") {
+      currIcon.src = "https://i.ibb.co/Kzkk59k/15.png";
+  } else if (icons === "rain") {
+      currIcon.src = "https://i.ibb.co/kBd2NTS/39.png";
+  } else if (icons === "clear-day") {
+      currIcon.src = "https://i.ibb.co/rb4rrJL/26.png";
+  } else if (icons === "clear-night") {
+      currIcon.src = "https://i.ibb.co/1nxNGHL/10.png";
+  }
+}
+// code to display the weekly icons change
 
 //code to display uv index value category
 function displayUvIndex(index) {
@@ -239,36 +263,70 @@ function displayUvIndex(index) {
   } else if (index > 10) {
     uvCategory = "Extreme";
   }
-
   uvElement.textContent = `${uvCategory}`;
 }
+
 //code to display humidity value
 function displayHumidity(humidity){
   const humidityElement=document.querySelector('.record3')
   let humiValue='';
   if(humidity>=0 && humidity<30){
     humiValue='Low'
-  }else if(humidity>=31 && humidity<60){
+  }else if(humidity>=31 && humidity<=60){
     humiValue='Modearte'
-  }else if (humidity>=61 && humidity<70){
+  }else if (humidity>=61 && humidity<=70){
     humiValue='High'
-  }else if (humidity>=71 && humidity<90){
+  }else if (humidity>=71 && humidity<=90){
     humiValue='Very High'
+  }else if(humidity>=91 && humidity<=100){
+    humiValue="Hazardous"
   }
   humidityElement.textContent=`${humiValue}`
 }
+
 //code to display visibility
 function displayVisibility(visibility){
   const visibilityElement=document.querySelector(".record4")
-  let visibilityValue='';
-  if(visibility>=0 && visibility<=10){
-    visibilityValue='Very Poor'
-  }else if(visibility>=11 && visibility<=20){
-    visibilityValue='Un Healthy'
-  }else if(visibility>=21 && visibility<=30){
-    visibilityValue='Fair'
-  }else if(visibility>=31 && visibility<=40){
-    visibilityValue='Clear Air'
+  let visibilityValue=''; 
+  if(visibility>=0 && visibility<=0.03){
+    visibilityValue='Dense Fog'
+  }else if(visibility>=0.04 && visibility<=0.16){
+    visibilityValue='Moderate Fog'
+  }else if(visibility>=0.17 && visibility<=0.35){
+    visibilityValue='Light Fog'
+  }else if(visibility>=0.36 && visibility<=1.13){
+    visibilityValue='Very Light Fog'
+  }else if(visibility>=1.14 && visibility<=2.16){
+    visibilityValue="Light Mist"
+  }else if(visibility>=2.17 && visibility<=5.4){
+    visibilityValue="Very Light Mist"
+  }else if(visibility>5.41 && visibility<=10.8){
+    visibilityValue="Clear Air"
+  }else if(visibility>=10.81 && visibility> 10.81){
+    visibilityValue="Very Clear Air"
   }
   visibilityElement.textContent=`${visibilityValue}`
+}
+
+//code to display Air quality
+
+function displayAirQuality(airquality1){
+const airquality=document.querySelector(".airqua")
+  let airqualityValue=''
+  if(airquality1>=0 && airquality1<=0.03){
+    airqualityValue="Good👌"
+  }else if(airquality1<=0.04 && airquality1<=0.16){
+    airqualityValue="Moderate 😐"
+  }else if(airquality1<=0.17 && airquality1<=0.35){
+    airqualityValue="Unhealthy for Sensitive Groups 😷"
+  }else if(airquality1<=0.36 && airquality1<=1.13){
+    airqualityValue="Unhealthy 😷"
+  }else if(airquality1<=1.14 && airquality1<=2.16){
+    airqualityValue="Very Unhealthy 😨"
+  }else if(airquality1<=2.17 && airquality1<=5.4){
+    airqualityValue="Hazardous 😱"
+  }else if(airquality1<=5.41 && airquality1<=10.8){
+  }else if(airquality1<=10.81 && airquality1> 10.81){
+  }
+  airquality.textContent=`${airqualityValue}`
 }
